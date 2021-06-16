68c6892a882af6591fc6a8f0a8f782c409dd80e5
Sefi123 <safeerahmad2050@gmail.com>
2021-06-16 13:17:16 +0500

    update

--- routes/faculty.js
+++ routes/faculty.js
@@ -5,8 +5,16 @@ var detail= require('../models/detail');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     let details= await detail.find();
-    console.log(details);
   res.render('faculty/list', {title : "Faculty Details", details});
 });
+
+router.get('/add', async function(req, res, next) {
+res.render('faculty/add');
+});
+
+router.post('/add', async function(req, res, next) {
+  console.log(req.body);
+  res.render('faculty/add');
+  });
 
 module.exports = router;
